5026aced8d4c613e3871ab6e552bca7bc6341008
kacpergasiorowski <kacper.gasiorowski.shyhoo@gmail.com>
2022-11-18 22:00:08 +0100

    Działająca funkcja liczenia paliwa na ilość okrążeń

--- app/src/main/java/com/example/racecompanion/MainActivity.kt
+++ app/src/main/java/com/example/racecompanion/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.racecompanion
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,14 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun kilk(view: View) {
-        val laps = findViewById<EditText>(R.id.etLaps).text.toString().toDouble()
-        val liters = findViewById<EditText>(R.id.etLiters).text.toString().toDouble()
-        val calc = laps * liters
         val apply = findViewById<TextView>(R.id.tvDisplay).apply {
-            text = calc.toString()
+            text = ""
+        }
+    }
+
+    fun kilk(view: View) {
+        val editText = findViewById<EditText>(R.id.etLaps)
+        val editText2 = findViewById<EditText>(R.id.etLiters)
+        if (TextUtils.isEmpty(editText.getText().toString())) {
+            val apply = findViewById<TextView>(R.id.tvDisplay).apply {
+                text = "Please enter the number of laps"
+            }
+        }
+        else if (TextUtils.isEmpty(editText2.getText().toString())) {
+            val apply = findViewById<TextView>(R.id.tvDisplay).apply {
+                text = "Please enter Litres per Lap"
+            }
+        }
+        else {
+            val laps = findViewById<EditText>(R.id.etLaps).text.toString().toDouble()
+            val liters = findViewById<EditText>(R.id.etLiters).text.toString().toDouble()
+            var lapsX = 0 + laps
+            var litersX = 0 + liters
+            val calc = lapsX * litersX + litersX
+            val apply = findViewById<TextView>(R.id.tvDisplay).apply {
+                text = calc.toString() + " liters for " + lapsX + " +1 laps."
+            }
         }
     }
 }
-/**XD*/

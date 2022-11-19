ec1cf4dd36a35fa6813e683568a2dcf20ab0c6ef
kacpergasiorowski <kacper.gasiorowski.shyhoo@gmail.com>
2022-11-19 13:13:05 +0100

    Dodano aktywności

---
+++ app/src/main/java/com/example/racecompanion/RaceLapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.racecompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RaceLapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_race_laps)
+    }
+}
---
+++ app/src/main/java/com/example/racecompanion/RaceTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.racecompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RaceTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_race_time)
+    }
+}
